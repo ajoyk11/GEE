fe19a37dcee7acaf317eedbea5779e431000d3ff
Ajoy Karmakar <166382489+ajoyk11@users.noreply.github.com>
2025-11-27 17:12:37 +0530

    Create First_and_last_image_date.js

---
+++ First_and_last_image_date.js
@@ -0,0 +1,19 @@
+var aoi = ee.Geometry.Rectangle([88.50, 26, 90.50, 27.00]);
+
+Map.addLayer(aoi)
+Map.centerObject(aoi)
+
+// Load the ImageCollection
+var ic = ee.ImageCollection("LANDSAT/LC09/C02/T1_TOA")
+.filterBounds(aoi)
+// .filterDate('2021-01-01', '2023-12-31'); // Adjust date range//
+
+// Get the first image and its date
+var firstImg = ic.sort('system:time_start', true).first();
+var firstDate = ee.Date(firstImg.get('system:time_start'));
+print('First Date:', firstDate);
+
+// Get the last image and its date
+var lastImg = ic.sort('system:time_start', false).first();
+var lastDate = ee.Date(lastImg.get('system:time_start'));
+print('Last Date:', lastDate);
