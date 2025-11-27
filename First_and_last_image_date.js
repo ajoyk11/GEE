e95d1893f383ee5dd8bb337b074fc6719c6d40d2
Ajoy Karmakar <166382489+ajoyk11@users.noreply.github.com>
2025-11-27 17:13:43 +0530

    Update First_and_last_image_date.js

--- First_and_last_image_date.js
+++ First_and_last_image_date.js
@@ -1,4 +1,4 @@
-var aoi = ee.Geometry.Rectangle([88.50, 26, 90.50, 27.00]);
+var aoi = ee.Geometry.Rectangle([88.50, 26, 90.50, 27.00])
 
 Map.addLayer(aoi)
 Map.centerObject(aoi)
@@ -7,12 +7,12 @@ Map.centerObject(aoi)
 var ic = ee.ImageCollection("LANDSAT/LC09/C02/T1_TOA")
 .filterBounds(aoi)
 
-// Get the first image and its date
-var firstImg = ic.sort('system:time_start', true).first();
-var firstDate = ee.Date(firstImg.get('system:time_start'));
-print('First Date:', firstDate);
+// Get the first image && its date
+var firstImg = ic.sort('system:time_start', true).first()
+var firstDate = ee.Date(firstImg.get('system:time_start'))
+print('First Date:', firstDate)
 
-// Get the last image and its date
-var lastImg = ic.sort('system:time_start', false).first();
-var lastDate = ee.Date(lastImg.get('system:time_start'));
-print('Last Date:', lastDate);
+// Get the last image & its date
+var lastImg = ic.sort('system:time_start', false).first()
+var lastDate = ee.Date(lastImg.get('system:time_start'))
+print('Last Date:', lastDate)
